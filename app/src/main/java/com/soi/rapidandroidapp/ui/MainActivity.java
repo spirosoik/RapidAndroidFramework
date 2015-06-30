f967f68b6dc6b4ef12775bcd5290703f8dfaadbc
Spiros Oikonomakis <spiros.oikonomakis@gmail.com>
2015-06-30 12:56:12 +0300

    Additions for back button

--- app/src/main/java/com/soi/rapidandroidapp/ui/MainActivity.java
+++ app/src/main/java/com/soi/rapidandroidapp/ui/MainActivity.java
@@ -1,11 +1,19 @@
 package com.soi.rapidandroidapp.ui;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
 import com.soi.rapidandroidapp.ui.common.AbstractFragmentActivity;
+import com.soi.rapidandroidapp.ui.common.BaseFragment;
 import com.soi.rapidandroidapp.ui.fragments.HomeFragment;
 
 public class MainActivity extends AbstractFragmentActivity {
+
+    /**
+     * This si the fragment which is now into the fragment container
+     * and it's shown up to the user
+     */
+    private BaseFragment currentFragment;
 
     @Override
     public void initListeners() {
@@ -18,9 +26,21 @@ public class MainActivity extends AbstractFragmentActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        if (currentFragment.allowBackPressed()) {
+            currentFragment.onBackPressed();
+        } else if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        createFragment(HomeFragment.newInstance(null), HomeFragment.class.getName(), true, false);
+        currentFragment = HomeFragment.newInstance(null);
+        createFragment(currentFragment, HomeFragment.class.getName(), true, false);
     }
 }
