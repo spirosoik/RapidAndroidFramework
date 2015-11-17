e803fb3ea59dc749748cca89768ead666c2d23a8
Spiros Oikonomakis <spiros.oikonomakis@gmail.com>
2015-11-17 14:41:04 +0200

    [FIXED] Fix Circle-ci tests with Android 23 & Roblectric 3.0

--- app/src/test/java/com/soi/rapidandroidapp/api/managers/FoursquareApiManagerTest.java
+++ app/src/test/java/com/soi/rapidandroidapp/api/managers/FoursquareApiManagerTest.java
@@ -1,7 +1,6 @@
 package com.soi.rapidandroidapp.api.managers;
 
-import android.test.InstrumentationTestCase;
-
+import android.os.Build;
 import com.soi.rapidandroidapp.TestBaseApplication;
 import com.soi.rapidandroidapp.api.managers.events.FoursquareExploreEvent;
 import com.soi.rapidandroidapp.api.managers.events.FoursquareSearchEvent;
@@ -13,20 +12,14 @@ import com.soi.rapidandroidapp.events.common.BusProvider;
 import com.soi.rapidandroidapp.test.support.UnitTestSpecification;
 import com.soi.rapidandroidapp.utilities.Constants;
 import com.squareup.otto.Subscribe;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.MockitoAnnotations;
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
-
 import java.util.Date;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-
 import javax.inject.Inject;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.annotation.Config;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
@@ -35,92 +28,85 @@ import static org.fest.assertions.api.Assertions.assertThat;
  * <p/>
  * This class handles all the api requests
  */
-@Config(emulateSdk = 18)
-public class FoursquareApiManagerTest extends UnitTestSpecification {
-    
-    @Inject
-    FoursquareApiManager foursquareApiManager;
-    
-    private String intent;
-    private Integer radius;
-    private String ll;
-    private String clientId;
-    private String clientSecret;
-    private int limit;
-    private int sortByDistance;
-    private String v;
-    private CountDownLatch signal;
+@Config(sdk = Build.VERSION_CODES.JELLY_BEAN)
+public class FoursquareApiManagerTest
+    extends UnitTestSpecification {
 
-    @Before
-    public void setup() throws Exception
-    {
+  @Inject FoursquareApiManager foursquareApiManager;
 
-        MockitoAnnotations.initMocks(this);
+  private String intent;
+  private Integer radius;
+  private String ll;
+  private String clientId;
+  private String clientSecret;
+  private int limit;
+  private int sortByDistance;
+  private String v;
+  private CountDownLatch signal;
 
-        TestBaseApplication.injectMocks(this);
+  @Before
+  public void setup() throws Exception {
 
-        intent = "global";
-        radius = 5000;
-        ll = "40.7,-74";
-        clientId = Constants.FOURSQUARE_API_CLIENT_ID;
-        clientSecret = Constants.FOURSQUARE_API_CLIENT_SECRET;
-        limit = 15;
-        sortByDistance = 1;
-        v = FoursquareApiManager.dateFormatter.format(new Date());
-    }
+    MockitoAnnotations.initMocks(this);
 
-    @Test
-    public void testFoursquareExplore() throws Throwable {
-        signal = new CountDownLatch(1);
-        new Runnable() {
-            @Override
-            public void run() {
-                foursquareApiManager.explore(ll, clientId, clientSecret, limit, sortByDistance, v);
-                BusProvider.getInstance().register(new Object() {
-                    @Subscribe
-                    public void onFoursquareExplore(FoursquareExploreEvent event) {
-                        signal.countDown();
-                        assertThat(event).isNotNull();
+    TestBaseApplication.injectMocks(this);
 
-                        Explore explore = event.response;
-                        assertThat(explore).isNotNull();
+    intent = "global";
+    radius = 5000;
+    ll = "40.7,-74";
+    clientId = Constants.FOURSQUARE_API_CLIENT_ID;
+    clientSecret = Constants.FOURSQUARE_API_CLIENT_SECRET;
+    limit = 15;
+    sortByDistance = 1;
+    v = FoursquareApiManager.dateFormatter.format(new Date());
+  }
 
-                        Response response = explore.response;
-                        assertThat(response).isNotNull();
-                        assertThat(response.groups).isNotNull();
-                    }
-                });
+  @Test
+  public void testFoursquareExplore() throws Throwable {
+    signal = new CountDownLatch(1);
+    new Runnable() {
+      @Override public void run() {
+        foursquareApiManager.explore(ll, clientId, clientSecret, limit, sortByDistance, v);
+        BusProvider.getInstance().register(new Object() {
+          @Subscribe public void onFoursquareExplore(FoursquareExploreEvent event) {
+            signal.countDown();
+            assertThat(event).isNotNull();
 
-            }
-        };
-        signal.await(15, TimeUnit.SECONDS);
-    }
+            Explore explore = event.response;
+            assertThat(explore).isNotNull();
 
-    @Test
-    public void testFoursquareSearch() throws Throwable {
-        signal = new CountDownLatch(1);
-        new Runnable() {
-            @Override
-            public void run() {
-                foursquareApiManager.search(intent, radius, ll, clientId, clientSecret, "ath", limit, sortByDistance, v);
-                BusProvider.getInstance().register(new Object() {
-                    @Subscribe
-                    public void onFoursquareExplore(FoursquareSearchEvent event) {
-                        signal.countDown();
-                        assertThat(event).isNotNull();
+            Response response = explore.response;
+            assertThat(response).isNotNull();
+            assertThat(response.groups).isNotNull();
+          }
+        });
+      }
+    };
+    signal.await(15, TimeUnit.SECONDS);
+  }
 
-                        SearchResult searchResult = event.response;
-                        assertThat(searchResult).isNotNull();
+  @Test
+  public void testFoursquareSearch() throws Throwable {
+    signal = new CountDownLatch(1);
+    new Runnable() {
+      @Override public void run() {
+        foursquareApiManager.search(intent, radius, ll, clientId, clientSecret, "ath", limit,
+            sortByDistance, v);
+        BusProvider.getInstance().register(new Object() {
+          @Subscribe public void onFoursquareExplore(FoursquareSearchEvent event) {
+            signal.countDown();
+            assertThat(event).isNotNull();
 
-                        SearchResponse response = searchResult.response;
-                        assertThat(response).isNotNull();
-                        assertThat(response.venues).isNotNull();
-                    }
-                });
+            SearchResult searchResult = event.response;
+            assertThat(searchResult).isNotNull();
 
-            }
-        };
-        signal.await(15, TimeUnit.SECONDS);
-    }
-
+            SearchResponse response = searchResult.response;
+            assertThat(response).isNotNull();
+            assertThat(response.venues).isNotNull();
+          }
+        });
+      }
+    };
+    signal.await(15, TimeUnit.SECONDS);
+  }
 }
